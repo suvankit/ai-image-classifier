8774e1008a55b0f3d7dd30eb27db848b38bdbb69
suvankit <51695526+suvankit@users.noreply.github.com>
2024-08-27 02:04:43 +0530

    removed keras importing

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from tensorflow import keras
 import numpy as np
 from PIL import Image, ImageOps
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
+st.markdown('# Hello Aliens!')
 img_size = 48
 
 model = load_model("AIGeneratedModel.h5")
